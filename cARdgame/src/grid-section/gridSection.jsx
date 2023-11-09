af043e8c878b379b9dd4c93980aa473156263341
JaviviDevs <javierexmar@gmail.com>
2023-11-10 00:49:32 +0100

    Se ha mejorado el grid de las imágenes

--- cARdgame/src/grid-section/gridSection.jsx
+++ cARdgame/src/grid-section/gridSection.jsx
@@ -5,7 +5,7 @@ export function GridCatalogo({title,imagenes,id}) {
   return (
     <article className='catalogo' id={id}>
     <h3 className='catalogo-h3'>{title}</h3>
-      <section className='catalogo-grid'>
+      <section className='catalogo-wrap'>
         {imagenes.map((imagen, index) => (
           <section className='catalogo-grid-containerImg' id={imagen[1]} key={index}>
               <img src={`./img/${imagen[0]}`} className={'catalogo-grid-img'}/>
